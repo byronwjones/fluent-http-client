a947dfdca2e8d18a3728f1de3fab651bb2d79aa8
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2021-10-22 21:01:18 -0400

    Add ability to retry, define acceptable status codes on request builder

--- BWJ.Net.Http.FluentHttpClient/RequestBuilder/HttpRequestBuilder.cs
+++ BWJ.Net.Http.FluentHttpClient/RequestBuilder/HttpRequestBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BWJ.Net.Http.RequestBuilder
@@ -25,6 +27,90 @@ namespace BWJ.Net.Http.RequestBuilder
             }
 
             _config.Headers.Add(name, value);
+            return this;
+        }
+
+        public HttpRequestBuilder AcceptStatusCodes(params object[] codes)
+        {
+            _config.AcceptStatusCodes.Clear();
+            _config.AcceptStatusCodeSeries.Clear();
+
+            foreach(var code in codes)
+            {
+                if(code is null)
+                {
+                    throw new ArgumentNullException("Null parameter encountered");
+                }
+                else if (code is HttpStatusCode)
+                {
+                    _config.AcceptStatusCodes.Add((int)code);
+                }
+                else if (code is string)
+                {
+                    var series = code.ToString().Trim();
+                    if(Regex.IsMatch(series, @"^[1-5]xx$", RegexOptions.IgnoreCase))
+                    {
+                        _config.AcceptStatusCodeSeries.Add(Convert.ToInt32(series[0]));
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"String parameter '{code}' is invalid");
+                    }
+                }
+                else if (IsInteger(code))
+                {
+                    var errMessage = $"Invalid status code {code}. Acceptable codes are 100-599";
+                    try
+                    {
+                        var intValue = (int)code;
+                        if (intValue > 99 && intValue < 600)
+                        {
+                            _config.AcceptStatusCodes.Add(intValue);
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException(nameof(codes), errMessage);
+                        }
+                    }
+                    catch
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(codes), errMessage);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException($"Status code parameter invalid: {code}");
+                }
+            }
+
+            return this;
+        }
+
+        public HttpRequestBuilder Retry(int numberOfTries, int delayBetweenTriesMs = 500)
+        {
+            if(numberOfTries < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfTries));
+            }
+            if(delayBetweenTriesMs < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(delayBetweenTriesMs));
+            }
+
+            _config.RetryCount = numberOfTries;
+            _config.RetryDelay = delayBetweenTriesMs;
+            return this;
+        }
+
+        public HttpRequestBuilder IncludeHeaders<T>(T headers)
+            where T : class
+        {
+            var dict = BuilderUtils.ToDictionary(headers);
+
+            foreach (var pair in dict)
+            {
+                _config.Headers.Add(pair.Key, pair.Value);
+            }
             return this;
         }
 
